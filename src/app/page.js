b8fa32f61b1839a305c3a949cd2b2ded4021fae1
elvitak <elvita.kalvane@gmail.com>
2024-09-17 14:28:23 +0200

    Add navigation bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import NavigationBar from "../components/NavigationBar";
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <NavigationBar />
+      <h6>Hello world</h6>
+      <h1>Hello world</h1>
+    </div>
+  );
 }
