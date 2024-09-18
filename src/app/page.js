8309425785738fdd0fbc0d2ec7eefe7845c0a8b3
elvitak <elvita.kalvane@gmail.com>
2024-09-18 13:19:01 +0200

    Connect to Sanity and get the data back from sanity

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
+import CardItem from "@/components/CardItem";
+import { getAllCelebrations } from "../../lib/api";
 import NavigationBar from "../components/NavigationBar";
 
-export default function Home() {
+export default async function Home() {
+  const celebrations = await getAllCelebrations();
   return (
     <div>
       <NavigationBar />
+      <div className="text-lg">Choose how to celebrate TODAY</div>
       <h6>Hello world</h6>
       <h1>Hello world</h1>
+      {celebrations.map((celebration) => (
+        <div key={celebration.slug}>
+          <CardItem />
+        </div>
+      ))}
     </div>
   );
 }
