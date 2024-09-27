8143bf3b9fc6aed0529fcf7088a510d67e33a79e
elvitak <elvita.kalvane@gmail.com>
2024-09-27 16:34:53 +0200

    Change class to className

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const CardItem = ({ image, title }) => {
   return (
-    <div class="p-4">
-      <div class="rounded overflow-hidden shadow-lg h-80 w-96 relative">
-        <img class="w-full h-56" src={image} />
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{title}</div>
-          <button class="bg-transparent hover:bg-black-500 text-black-700 font-semibold hover:text-white py-2 px-4 border border-black-500 hover:border-transparent rounded absolute bottom-2 right-2">
+    <div className="p-4">
+      <div className="rounded overflow-hidden shadow-lg h-80 w-96 relative">
+        <img className="w-full h-56" src={image} />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{title}</div>
+          <button className="bg-transparent hover:bg-black-500 text-black-700 font-semibold hover:text-white py-2 px-4 border border-black-500 hover:border-transparent rounded absolute bottom-2 right-2">
             Read More
           </button>
         </div>
