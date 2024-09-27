11a84fa722e6ec314f51c5898999bd5d12e6b105
elvitak <elvita.kalvane@gmail.com>
2024-09-27 07:46:08 +0200

    Add image and intro title to main page

--- src/components/MainText.js
+++ src/components/MainText.js
@@ -2,11 +2,19 @@ import React from "react";
 
 const MainText = () => {
   return (
-    <div>
-      <div class="text-4xl text-center text-gray-600 m-2">
-        Choose how to celebrate
+    <div class="flex justify-center items-center">
+      <div class="p-10">
+        <img
+          class="max-w-full h-auto"
+          src="https://m.media-amazon.com/images/I/6104OzuIQqL._SY522_.jpg"
+        />
       </div>
-      <div class="uppercase text-6xl text-center block">today</div>
+      <div>
+        <div class="text-4xl text-center text-gray-600 m-2">
+          Make every day a holiday with
+        </div>
+        <div class="uppercase text-6xl text-center block">celebrate today</div>
+      </div>
     </div>
   );
 };
