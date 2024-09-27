2a32098a0558e55ecb2758a49e589ca37438d195
elvitak <elvita.kalvane@gmail.com>
2024-09-27 12:27:55 +0200

    Add link placeholders for navigating to different pages, add flex to navigation bar and fix positions

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,19 +3,13 @@ import Link from "next/link";
 
 const NavigationBar = () => {
   return (
-    <div className="w-full bg-white">
-      <div className="max-w-screen-xl px-4 lg:px-0l">
-        <h1 className="text-3xl font-extrabold uppercase text-black text-left">
-          Celebrate Today
-        </h1>
-        <div className="md:inline-flex items-center text-grey-900 hover:text-black duration-200">
-          <Link
-            href="/about"
-            className="text-sm uppercase font-semibold relative"
-          >
-            About Project
-          </Link>
-        </div>
+    <div className="flex justify-end px-4 lg:px-01 w-full">
+      <h1 className="text-3xl font-extrabold  text-black">Celebrate Today</h1>
+      <div className="md:inline-flex text-grey-900 hover:text-black duration-200 pt-3 ml-auto">
+        <Link href="/">Today's celebrations</Link>
+        <Link href="/about" className="ml-3">
+          About Project
+        </Link>
       </div>
     </div>
   );
